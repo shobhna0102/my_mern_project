5c1f47e2b7c262e041f6bece0d501482e3f6b534
shobhna <shobhna@webcluesinfotech.com>
2022-09-26 18:49:33 +0530

    post like comment api commit

--- routes/posts.js
+++ routes/posts.js
@@ -102,7 +102,29 @@ router.get('/getpostbyid/:id',auth,async(req,res)=>{
 })
 
 
+//@route PUT api/postlike/:id
+//@desc like a post
+//@access Private
 
+router.put('/postlike',auth,async(req,res)=>{
+    try {
+        const post = await Posts.findById(req.params.id);
+
+        // Check if the post has already been liked
+        if (post.likes.some((like) => like.user.toString() === req.user.id)) {
+          return res.status(400).json({ msg: 'Post already liked' });
+        }
+    
+        post.likes.unshift({ user: req.user.id });
+    
+        await post.save();
+    
+        return res.json(post.likes);
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+      }
+    });
 
 
 module.exports=router
